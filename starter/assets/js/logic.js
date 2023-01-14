195b545bb9300a344c0ab59df1fccce64626cd39
Rob S <me@rob-saunders.co.uk>
2023-01-14 16:09:10 +0000

    Countdown timer created and updates HTML element

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -2,4 +2,27 @@
 
 var timer = document.querySelector("#time");
 
-timer.textContent = "Hello"
+
+
+// Create a timer function for the game with a variable start time
+var timeLeft = 5;
+
+function setTime() {
+
+    var timerInterval = setInterval(function() {
+        timer.textContent = timeLeft;
+  
+      if(timeLeft === 0) {
+        // Stops execution of action at set interval
+        timer.textContent = "Time's Up";
+        clearInterval(timerInterval);
+
+      };
+
+      // Reduce Time By 1 Second
+      timeLeft--;
+  
+    }, 1000);
+
+  };
+
