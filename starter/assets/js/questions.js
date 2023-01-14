c91e39819277ec53e15c82e4d06aced39fc048e8
Rob S <me@rob-saunders.co.uk>
2023-01-14 17:18:50 +0000

    changed the questions to an array of objects for randomisation

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,26 +1,29 @@
-const question1 =
+const questions = [
+
+
     {
         question: "Javascript is an _______ language?",
         wrongAnswer1: "A dinosaur",
         wrongAnswer2: "Object-based",
         wrongAnswer3: "Procedural",
         correctAnswer: "Object-Oriented"
-    };
+    },
 
-const question2 =
+
     {
         question: "Which of the following keywords is used to define a variable in Javascript?",
         wrongAnswer1: "Google",
         wrongAnswer2: "Brainpower",
         wrongAnswer3: "dictionary",
         correctAnswer: "let & var"
-    };
+    },
 
-const question3 =
+
     {
-        question: "Which of the following keywords is used to define a variable in Javascript?",
-        wrongAnswer1: "Google",
-        wrongAnswer2: "Brainpower",
-        wrongAnswer3: "dictionary",
-        correctAnswer: "let & var"
-    };
+        question: "In JavaScript, what is a block of statement?",
+        wrongAnswer1: "Conditional block",
+        wrongAnswer2: "block that contains a single statement",
+        wrongAnswer3: "both conditional block and a single statement",
+        correctAnswer: "block that combines a number of statements into a single compound statement"
+    },
+]
