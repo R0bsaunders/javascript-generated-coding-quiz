d02cafb16d214334bb9c76931a6b4732f35212f1
Rob S <me@rob-saunders.co.uk>
2023-01-14 16:59:55 +0000

    added 3 example questions as objects with question, wrong answers and correct answer keys

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,3 +1,26 @@
-const questions = [
-    
-]
+const question1 =
+    {
+        question: "Javascript is an _______ language?",
+        wrongAnswer1: "A dinosaur",
+        wrongAnswer2: "Object-based",
+        wrongAnswer3: "Procedural",
+        correctAnswer: "Object-Oriented"
+    };
+
+const question2 =
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        wrongAnswer1: "Google",
+        wrongAnswer2: "Brainpower",
+        wrongAnswer3: "dictionary",
+        correctAnswer: "let & var"
+    };
+
+const question3 =
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        wrongAnswer1: "Google",
+        wrongAnswer2: "Brainpower",
+        wrongAnswer3: "dictionary",
+        correctAnswer: "let & var"
+    };
